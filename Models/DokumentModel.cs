6e47d0d956ac5b057667cced5b4d57c65c479f4e
prdxt <dominosjaj@gmail.com>
2023-03-21 21:29:21 +0100

    Dodanie dziedziczenia do klasy FormatModel oraz dodanie do niej własciwości odpowiadające za formatowanie tekstu. Dodano dziedziczenie do klasy DokumentModel oraz własciwości reprezentujące kolejno: tekst w pliku, sciezkę pliku oraz nazwe pliku. Dodano również właściwość sprawdzającą czy scieżka lub nazwa są puste

--- Models/DokumentModel.cs
+++ Models/DokumentModel.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace OmatkoboskaNotatnik.Models
 {
-    public class DokumentModel
+    public class DokumentModel : ObservableObject
     {
+        private string _text;
+        public string Text
+        {
+            get 
+            {
+                return _text; 
+            }
+            set 
+            { 
+                OnPropertyChanged(ref _text, value);
+
+
+            }
+
+        }
+
+        private string _fileSciezka;
+        public string FileSciezka
+        {
+            get
+            {
+                return _fileSciezka;
+            }
+            set
+            {
+                OnPropertyChanged(ref _fileSciezka, value);
+
+
+            }
+
+        }
+
+        private string _nazwaPliku;
+        public string NazwaPliku
+        {
+            get
+            {
+                return _nazwaPliku;
+            }
+            set
+            {
+                OnPropertyChanged(ref _nazwaPliku, value);
+
+
+            }
+
+        }
+        public bool isEmpty
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(NazwaPliku) || string.IsNullOrEmpty(FileSciezka))
+                    return true;
+                return false;
+            }
+        }
+
     }
 }
