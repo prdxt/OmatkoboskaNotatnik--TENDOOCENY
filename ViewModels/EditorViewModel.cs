4a260cf8434923bd09893249adb77149d2bf8102
prdxt <dominosjaj@gmail.com>
2023-03-22 15:30:25 +0100

    Dodanie metody która otwiera dialog do wyboru stylów. Zmiana sposobu formatowania tekstu itp.

---
+++ ViewModels/EditorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using OmatkoboskaNotatnik.Models;
+using System;
+using System.Windows.Input;
+
+namespace OmatkoboskaNotatnik.ViewModels
+{
+    public class EditorViewModel
+    {
+        public ICommand FormatCommand { get; }
+        public ICommand WrapCommand { get; }
+        public FormatModel Format {get; set;}
+        public DokumentModel Dokument { get; set;}
+
+        public EditorViewModel(DokumentModel dokument)
+        {
+            Dokument = dokument;
+            Format = new FormatModel();
+            FormatCommand = new RelayCommand(OpenStyleDialog);
+            WrapCommand = new RelayCommand(ToggleWrap);
+            
+        }
+
+        private void OpenStyleDialog()
+        {
+            //otwieramy tu wybór stylow 
+            throw new NotImplementedException();
+        }
+
+        private void ToggleWrap()
+        {
+            if (Format.Wrap == System.Windows.TextWrapping.Wrap)
+                Format.Wrap = System.Windows.TextWrapping.NoWrap;
+            else
+                Format.Wrap = System.Windows.TextWrapping.Wrap;
+
+        }
+
+    }
+}
