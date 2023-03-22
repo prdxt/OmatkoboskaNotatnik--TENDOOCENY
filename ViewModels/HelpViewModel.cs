d9c6537ec1ddebe4f6f5e8a4cabe4e96882d4e5e
prdxt <dominosjaj@gmail.com>
2023-03-22 16:27:06 +0100

    Dodanie tej dziwnej opcji "pomoc" co jest w notatniku oraz definiowanie widoku dla głownego okna aplikacji.

---
+++ ViewModels/HelpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OmatkoboskaNotatnik.ViewModels
+{
+    public class HelpViewModel : ObservableObject
+    {
+        public ICommand HelpCommand { get; }
+        public HelpViewModel()
+        {
+            HelpCommand = new RelayCommand(null);
+        }
+        private void DisplayAbout()
+        {
+            //to smieszne z pomocą
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using OmatkoboskaNotatnik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmatkoboskaNotatnik.ViewModels
+{
+    public class MainViewModel
+    {
+        private DokumentModel _dokument;
+        public EditorViewModel Editor { get; set; }
+        public FileViewModel File { get; set; }
+        public HelpViewModel Help { get; set; }
+
+        public MainViewModel()
+        {
+            _dokument = new DokumentModel();
+            Help = new HelpViewModel();
+            Editor = new EditorViewModel(_dokument);
+            File = new FileViewModel(_dokument);
+
+        }
+    }
+}
